9b9521d02590e0f4de94c0967725fe222994c3cf
Florian Tautz <dev@nyronet.de>
2021-11-08 18:57:38 +0100

    add logging

--- weather_receiver/core.py
+++ weather_receiver/core.py
@@ -1,11 +1,16 @@
 # Copyright 2021 Florian Tautz
+
 import contextlib
+import logging
 from datetime import datetime
 
 import serial
 import requests
 
+
 SERIAL_TIMEOUT = 1
+
+logger = logging.getLogger(__name__)
 
 
 class WeatherReceiver:
@@ -24,8 +29,15 @@ class WeatherReceiver:
                 if len(raw_line) == 0:
                     continue
                 line = datetime.utcnow().isoformat() + raw_line.decode("ascii").rstrip()
-                fp.write(line + "\n")
-                requests.post(self.api_url, data=line)
+                logger.info("received " + line)
+                try:
+                    fp.write(line + "\n")
+                except:
+                    logger.exception("failed to save measurement to file")
+                try:
+                    requests.post(self.api_url, data=line)
+                except:
+                    logger.exception("failed to submit measurement to server")
 
 
 def main():
@@ -33,8 +45,20 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("serial_port")
     parser.add_argument("api_url")
-    parser.add_argument("--file")
+    parser.add_argument("--file", default="weather_data.txt")
+    parser.add_argument("--log", default="weather_receiver.log")
     args = parser.parse_args()
+
+    log_format = "[%(asctime)s] <%(levelname)s> %(name)s: %(message)s"
+    time_format = "%Y-%m-%d %H:%M:%S%z"
+
+    logging.basicConfig(level=logging.DEBUG, format=log_format, datefmt=time_format)
+
+    logfile_formatter = logging.Formatter(log_format, time_format)
+    logfile_handler = logging.FileHandler(args.log)
+    logfile_handler.setFormatter(logfile_formatter)
+    logfile_handler.setLevel(logging.INFO)
+    logging.getLogger().addHandler(logfile_handler)
 
     receiver = WeatherReceiver(args.serial_port, args.api_url, args.file)
     receiver.run()
